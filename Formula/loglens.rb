cc47ee8a6980015b6db7dd0be1604756cf3cb71a
Your Name <you@example.com>
2025-11-21 15:55:03 +0100

    new v

--- Formula/loglens.rb
+++ Formula/loglens.rb
@@ -1,25 +1,25 @@
 class Loglens < Formula
   desc "A blazingly fast, structured log query tool"
   homepage "https://getloglens.com"
-  version "1.6.0" # <-- Updated to 1.6.0
+  version "1.7.0"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://download.getloglens.com/v1.6.0/loglens-x86_64-macos.tar.gz"
-      sha256 "2f22f465a006d5379fd3a174a548cf31fe3519683196d027915face441f5596b"
+      url "https://download.getloglens.com/v1.7.0/loglens-x86_64-macos.tar.gz"
+      sha256 "bfdbdd1caacbb0fa3974d2ea609ad3247aaa6f6793713631ff9aab1c7e029fc6"
     elsif Hardware::CPU.arm?
-      url "https://download.getloglens.com/v1.6.0/loglens-aarch64-macos.tar.gz"
-      sha256 "74d881a089dce7f26d95b285307164d9fd3c23347944447474507acdb7a95b19"
+      url "https://download.getloglens.com/v1.7.0/loglens-aarch64-macos.tar.gz"
+      sha256 "4bf54fbe220be7d6c8e19668e02dee3acd6fcd7d60109c97d92f7ae56ceeef64"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://download.getloglens.com/v1.6.0/loglens-x86_64-linux.tar.gz"
-      sha256 "4a47acbad7352def6aa3f88b77231a798584ba1c94c14f546941da40b085aa5d"
+      url "https://download.getloglens.com/v1.7.0/loglens-x86_64-linux.tar.gz"
+      sha256 "7839bc0b3e4d3d005f7d954c86ac446854410c038856d7943cfb0ba6ac6c61b8"
     elsif Hardware::CPU.arm?
-      url "https://download.getloglens.com/v1.6.0/loglens-aarch64-linux.tar.gz"
+      url "https://download.getloglens.com/v1.7.0/loglens-aarch64-linux.tar.gz"
       sha256 "9eb52ee46c7ab5ea4ca0982415da99fded1b7d7354f75e50847bdae6cb44eb66"
     end
   end
